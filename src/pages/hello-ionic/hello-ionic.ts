30c964620aa977fd8ee20eeef9ea0bdbfa18024a
Nick Redmond <nick.redmond@dealertrack.com>
2017-01-19 12:45:32 -0700

    Initial commit of a working Ionic app

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  constructor() {
+  	
+  }
+  ionViewDidLoad() {
+  	console.log("blessing");
+  	let canvas = <HTMLCanvasElement>document.getElementById("mainCanvas");
+  	var position = { x: 20, y: 20 }; //event.center;
+    let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    ctx.beginPath();
+    ctx.arc(position.x, position.y, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = '#00DD00';
+    ctx.fill();
+  }
+}
---
+++ src/pages/list/list.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    this.icons = ['heart', 'heart-outline', 'color-wand', 'brush', 'book', 'bulb',
+    'cafe', 'chatbubbles', 'restaurant', 'pizza', 'color-wand'];
+    this.items = [];
+
+    let numWands = 0;
+    let numFoods = 0;
+    while (this.icons.length > 0) {
+    //for(let i = 1; i < 11; i++) {
+      let randomIndex = Math.floor(Math.random() * this.icons.length);
+      let randomIcon = this.icons[randomIndex];
+      this.icons.splice(randomIndex, 1);
+      let ititle = 'None';
+      let inote = 'None';
+
+      switch (randomIcon) {
+      case 'heart':
+        ititle = 'Love';
+        inote = 'she is my Goo';
+        break;
+      case 'heart-outline':
+        ititle = 'Love';
+        inote = 'She is my Special'
+        break;
+      case 'color-wand':
+        ititle = 'Harry Potter';
+        inote = 'she ' + 'really '.repeat(numWands) + 'likes him';
+        numWands++;
+        break;
+      case 'brush':
+        ititle = 'Art';
+        inote = 'we both like art';
+        break;
+      case 'book':
+        ititle = 'Books';
+        inote = 'we both like reading';
+        break;
+      case 'cafe':
+        ititle = 'Talking';
+        inote = 'nice to be around her :)'
+        break;
+      case 'pizza':
+        ititle = 'Food';
+        inote = 'omg we ' + 'really '.repeat(numFoods) + 'love it';
+        numFoods++;
+        break;
+      case 'restaurant':
+        ititle = 'Food';
+        inote = 'omg we ' + 'really '.repeat(numFoods) + 'love it';
+        numFoods++;
+        break;
+      case 'chatbubbles':
+        ititle = 'Talking';
+        inote = 'if only we were politicians';
+        break;
+      case 'bulb':
+        ititle = 'Critical Thinking';
+        inote = 'together, we make a whole';
+        break;
+      }
+
+      this.items.push({
+        title: ititle,
+        note: inote,
+        icon: randomIcon
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
+  }
+}
