0c2c8643ce6bf5172d1292415893729f2d0314a1
Nick Redmond <nick.redmond@dealertrack.com>
2017-01-31 10:51:41 -0700

    Now, BallVsWild page is building gesture data that can be used for shooting projectiles (from the Hero).

---
+++ src/models/position.tuple.ts
@@ -0,0 +1,9 @@
+export class PositionTuple {
+	x: Number;
+	y: Number;
+
+	constructor(x: Number = 0, y: Number = 0){
+		this.x = x;
+		this.y = y;
+	}
+}
--- src/pages/ball-vs-wild/ball-vs-wild.ts
+++ src/pages/ball-vs-wild/ball-vs-wild.ts
@@ -2,19 +2,50 @@ import { Component } from "@angular/core";
 import { Unit } from "../../models/unit";
 import { Color } from "../../models/color";
 import { Circle } from "../../models/shapes";
+import { PositionTuple } from "../../models/position.tuple";
+import { ExtendedMath } from "../../models/extendedmath.ts";
 
 @Component({
   selector: 'ball-vs-wild',
   templateUrl: 'ball-vs-wild.html'
 })
 export class BallVsWildPage {
+  xVelocities: number[] = [];
+  yVelocities: number[] = [];
+  startPosition: PositionTuple = null;
+  endPosition: PositionTuple = null;
+  isTouching: Boolean = false;
   constructor() {}
 
   onTouchStart(event) {
-  	console.log('starting: ' + JSON.stringify(Object.keys(event)));
+  	//console.log('starting: ' + JSON.stringify(Object.keys(event)));
+    this.isTouching = true;
+  }
+  onDragGesture(event){
+    let centerX = event.center.x;
+    let centerY = event.center.y;
+    if (this.startPosition === null){
+      this.startPosition = new PositionTuple(centerX, centerY);
+    }
+    this.endPosition = new PositionTuple(centerX, centerY);
+
+    this.xVelocities.push(event.velocityX);
+    this.yVelocities.push(event.velocityY);
   }
   onTouchEnd(event) {
+    let averageVelocityX = ExtendedMath.average(this.xVelocities);
+    let averageVelocityY = ExtendedMath.average(this.yVelocities);
 
+    console.log("swing it: (" + averageVelocityX + ", " + averageVelocityY +
+      "), (" + this.startPosition.x + ", " + this.startPosition.y + ") -> (" +
+      this.endPosition.x + ", " + this.endPosition.y + ")");
+
+    //console.log('ending: ' + JSON.stringify(Object.keys(event)));
+    this.xVelocities = [];
+    this.yVelocities = [];
+    this.startPosition = null;
+    this.endPosition = null;
+    this.isTouching = false;
   }
 
   ionViewDidEnter() {
