bf2d326de9aef67c50de994a4725aa17843ddbd0
Nick Redmond <nick.redmond@dealertrack.com>
2017-02-11 15:28:36 -0700

    Implemented two separate high-scores lists: 'daily' and 'all-time'. Set up a cron job (outside of this repo) that empties the 'daily' list, well, once per day. 'all-time' list always takes priority over 'daily' list, in terms of ranking.

---
+++ src/models/graphic.artist.ts
@@ -0,0 +1,45 @@
+import { BallVsWildPage } from "../pages/ball-vs-wild/ball-vs-wild";
+
+export class GraphicArtist {
+	static drawHighScores(page: BallVsWildPage, ctx: CanvasRenderingContext2D,
+			scoresList: Object, centerX: number, topY: number, isLastList = false) {
+		page.userName = "";
+        document.getElementById("usernameField").style.display = "none";
+        let scores = scoresList["scores"];
+
+        if (ctx.font != "28px Courier" || ctx.textAlign != "center") {
+          ctx.font = "28px Courier";
+          ctx.textAlign = "center";
+        }
+        ctx.fillText(scoresList["identifier"] + " HIGH SCORES", centerX, topY - 20);
+        ctx.fillRect(ctx.canvas.width * 0.1, topY, ctx.canvas.width * 0.8, 3);
+
+        ctx.font = "18px Courier";
+        if (isLastList) {
+        	ctx.fillText("(Tap to retry)", centerX, topY + 50 + (scores.length * 25));
+        }
+
+        ctx.textAlign = "left";
+        let leftMargin = ctx.canvas.width * 0.1;
+
+        scores.sort(function(a, b){
+          let result = 0;
+          if (a["score"] < b["score"]){
+            result = 1;
+          } else if (a["score"] > b["score"]) {
+            result = -1;
+          }i
+          return result;
+        });
+        for (var i = 0; i < scores.length; i++) {
+          ctx.fillText(scores[i]["name"], leftMargin, topY + 25 + (25 * i));
+        }
+
+        ctx.textAlign = "right";
+        let rightMargin = ctx.canvas.width * 0.9;
+
+        for (var i = 0; i < scores.length; i++) {
+          ctx.fillText(scores[i]["score"], rightMargin, topY + 25 + (25 * i));
+        }
+	}
+}
